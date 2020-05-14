af2293ffb3c00f16bd011f77c152fde5bc3f33f2
MichaelBengtsson <michael@krokedil.se>
2020-05-14 08:51:25 +0200

    Update example tests

--- tests/unit-tests/test-example.php
+++ tests/unit-tests/test-example.php
@@ -1,7 +1,23 @@
 <?php
 
-class Test_KCO_Cart_Helpers extends AKrokedil_Unit_Test_Case {
+class Test_Example extends AKrokedil_Unit_Test_Case {
 	public function test_example() {
 		$this->assertTrue( true );
+	}
+
+	public function create() {
+		return;
+	}
+
+	public function update() {
+		return;
+	}
+
+	public function view() {
+		return;
+	}
+
+	public function delete() {
+		return;
 	}
 }
