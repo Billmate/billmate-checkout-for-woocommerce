9befbc9a63b2adb3103e1d7b4788452fdfb43d52
MichaelBengtsson <michael@krokedil.se>
2020-05-14 08:47:43 +0200

    Add PHP Unit framework and github workflow

---
+++ tests/config.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Plugin configuration file.
+ *
+ * @author Krokedil
+ * @since 1.0.0
+ */
+return array(
+	'name'    => 'woocommerce-billmate-checkout.php',
+	'version' => '1.0.0',
+);
---
+++ tests/unit-tests/test-example.php
@@ -0,0 +1,7 @@
+<?php
+
+class Test_KCO_Cart_Helpers extends AKrokedil_Unit_Test_Case {
+	public function test_example() {
+		$this->assertTrue( true );
+	}
+}
