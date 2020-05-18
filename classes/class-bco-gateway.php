5cfa74105f7007415b670eb2d0f009899eb95c24
Kristian Andersson <32387117+Kristian-Andersson@users.noreply.github.com>
2020-05-18 14:19:53 +0200

    Add BCO_Gateway class

---
+++ classes/class-bco-gateway.php
@@ -0,0 +1,101 @@
+<?php
+/**
+ * Gateway class file.
+ *
+ * @package Billmate_Checkout/Classes
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
+/**
+ * Gateway class.
+ */
+class BCO_Gateway extends WC_Payment_Gateway {
+
+	/**
+	 * Class constructor.
+	 */
+	public function __construct() {
+		$this->id                 = 'bco';
+		$this->method_title       = __( 'Billmate Checkout', 'billmate-checkout-for-woocommerce' );
+		$this->icon               = '';
+		$this->method_description = __( 'Allows payments through ' . $this->method_title . '.', 'billmate-checkout-for-woocommerce' ); // phpcs:ignore
+
+		// Load the form fields.
+		$this->init_form_fields();
+
+		// Load the settings.
+		$this->init_settings();
+
+		// Define user set variables.
+		$this->enabled     = $this->get_option( 'enabled' );
+		$this->title       = $this->get_option( 'title' );
+		$this->description = $this->get_option( 'description' );
+		$this->debug       = $this->get_option( 'debug' );
+		$this->testmode    = 'yes' === $this->get_option( 'testmode' );
+
+		// Supports.
+		$this->supports = array(
+			'products',
+			'refunds',
+		);
+
+		// Actions.
+		add_action( 'woocommerce_update_options_payment_gateways_' . $this->id, array( $this, 'process_admin_options' ) );
+	}
+
+	/**
+	 * Check if this gateway is enabled and available in the user's country.
+	 *
+	 * @return boolean
+	 */
+	public function is_available() {
+		if ( 'yes' === $this->enabled ) {
+			// Do checks here.
+			return true;
+		}
+		return false;
+	}
+
+	/**
+	 * Processes the WooCommerce Payment
+	 *
+	 * @param string $order_id The WooCommerce order ID.
+	 *
+	 * @return array
+	 */
+	public function process_payment( $order_id ) {
+		$order = wc_get_order( $order_id );
+
+		// Return pay for order redirect.
+		return array(
+			'result'   => 'success',
+			'redirect' => $order->get_checkout_payment_url( true ),
+		);
+	}
+
+	/**
+	 * Initialise Gateway Settings Form Fields.
+	 *
+	 * @return void
+	 */
+	public function init_form_fields() {
+		$this->form_fields = include BILLMATE_CHECKOUT_PATH . '/includes/bco-form-fields.php';
+	}
+
+}
+
+/**
+ * Add Billmate_Checkout payment gateway
+ *
+ * @wp_hook woocommerce_payment_gateways
+ * @param  array $methods All registered payment methods.
+ * @return array $methods All registered payment methods.
+ */
+function add_bco_method( $methods ) {
+	$methods[] = 'BCO_Gateway';
+	return $methods;
+}
+add_filter( 'woocommerce_payment_gateways', 'add_bco_method' );
