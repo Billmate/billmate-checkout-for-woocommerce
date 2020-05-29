70c1fb6d17dfddf042c2070d4dd04e843dc31921
Kristian Andersson <32387117+Kristian-Andersson@users.noreply.github.com>
2020-05-29 17:57:52 +0200

    Change function to static functions and add helper getter for request data

--- classes/requests/helpers/class-bco-order-cart-helper.php
+++ classes/requests/helpers/class-bco-order-cart-helper.php
@@ -13,75 +13,118 @@ if ( ! defined( 'ABSPATH' ) ) {
  * Order Cart helper class.
  */
 class BCO_Order_Cart_Helper {
+
+	/**
+	 * Get the order cart handling key value.
+	 *
+	 * @param WC_Order $order The WooCommerce order.
+	 * @return array
+	 */
+	public static function get_order_cart_handling( $order ) {
+		return array(
+			'withouttax' => 0,
+			'taxrate'    => self::get_handling_tax_rate( $order ),
+		);
+	}
+
+	/**
+	 * Get the order cart shipping key value.
+	 *
+	 * @param WC_Order $order The WooCommerce order.
+	 * @return array
+	 */
+	public static function get_order_cart_shipping( $order ) {
+		return array(
+			'withouttax' => self::get_shipping_without_tax( $order ),
+			'taxrate'    => self::get_shipping_tax_rate( $order ),
+		);
+	}
+
+	/**
+	 * Get the order cart total key value.
+	 *
+	 * @param WC_Order $order The WooCommerce order.
+	 * @return array
+	 */
+	public static function get_order_cart_total( $order ) {
+		return array(
+			'withouttax' => self::get_total_without_tax( $order ),
+			'tax'        => self::get_total_tax( $order ),
+			'rounding'   => 0,
+			'withtax'    => self::get_total_with_tax( $order ),
+		);
+	}
+
+
 	/**
 	 * Get order handling without tax.
 	 *
-	 * @param array $order_item order item.
-	 * @return int $item_price handling excl tax.
+	 * @param WC_Order $order The WooCommerce order.
+	 * @return int $handling_without_tax handling excl tax.
 	 */
-	public function get_handling_without_tax( $order_item ) {
-		return round( $order_item->get_total() * 100 );
+	public static function get_handling_without_tax( $order ) {
+		return round( ( $order->get_total() - $order->get_total_tax() ) * 100 );
 	}
 
 	/**
 	 * Get order handling tax rate.
 	 *
-	 * @param array $order_item order item.
-	 * @return int $item_price handling tax rate.
+	 * @param WC_Order $order The WooCommerce order.
+	 * @return int $handling_tax_rate handling tax rate.
 	 */
-	public function get_handling_tax_rate( $order_item ) {
-		$tax_rate = ( $order_item->get_total_tax() > 0 ) ? $order_item->get_total_tax() / $order_item->get_total() * 100 : 0;
+	public static function get_handling_tax_rate( $order ) {
+		$tax_rate = ( $order->get_total_tax() > 0 ) ? $order->get_total_tax() / ( $order->get_total() - $order->get_total_tax() ) * 100 : 0;
 		return round( $tax_rate );
 	}
 
 	/**
 	 * Get order shipping without tax.
 	 *
-	 * @param array $order_item order item.
-	 * @return int $item_price shipping excl tax.
+	 * @param WC_Order $order The WooCommerce order.
+	 * @return int $shipping_without_tax shipping excl tax.
 	 */
-	public function get_shipping_without_tax( $order_item ) {
-		return round( $order_item->get_total() * 100 );
+	public static function get_shipping_without_tax( $order ) {
+		return round( $order->get_shipping_total() * 100 );
 	}
 
 	/**
 	 * Get order shipping tax rate.
 	 *
-	 * @param array $order_item order item.
-	 * @return int $item_price shipping tax rate.
+	 * @param WC_Order $order The WooCommerce order.
+	 * @return int $shipping_tax_rate shipping tax rate.
 	 */
-	public function get_shipping_tax_rate( $order_item ) {
-		$tax_rate = ( $order_item->get_total_tax() > 0 ) ? $order_item->get_total_tax() / $order_item->get_total() * 100 : 0;
+	public static function get_shipping_tax_rate( $order ) {
+		$tax_rate = ( $order->get_shipping_tax() > 0 ) ? $order->get_shipping_tax() / $order->get_shipping_total() * 100 : 0;
 		return round( $tax_rate );
 	}
 
 	/**
 	 * Get order total excluding tax.
 	 *
-	 * @param array $order_item order item.
-	 * @return int $item_price order total excl tax.
+	 * @param WC_Order $order The WooCommerce order.
+	 * @return int $total_without_tax order total excl tax.
 	 */
-	public function get_total_without_tax( $order_item ) {
-		return round( $order_item->get_total() * 100 );
+	public static function get_total_without_tax( $order ) {
+		return round( ( $order->get_total() - $order->get_total_tax() ) * 100 );
 	}
 
 	/**
 	 * Get order total tax.
 	 *
-	 * @param array $order_item order item.
-	 * @return int $item_price order total tax.
+	 * @param WC_Order $order The WooCommerce order.
+	 * @return int $total_tax order total tax.
 	 */
-	public function get_total_tax( $order_item ) {
-		return round( $order_item->get_total_tax() * 100 );
+	public static function get_total_tax( $order ) {
+		return round( $order->get_total_tax() * 100 );
 	}
 
 	/**
 	 * Get order total inclusive tax.
 	 *
-	 * @param array $order_item order item.
-	 * @return int $order_total order total incl tax.
+	 * @param WC_Order $order The WooCommerce order.
+	 * @return int $total_with_tax order total incl tax.
 	 */
-	public function get_total_with_tax( $order_item ) {
-		return round( ( $order_item->get_total() + $order_item->get_total_tax() ) * 100 );
+	public static function get_total_with_tax( $order ) {
+		return round( $order->get_total() * 100 );
 	}
 }
