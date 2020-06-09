ea9d5287b03091c7bf82dbe7adf8cfa7bdd62dbb
Kristian Andersson <32387117+Kristian-Andersson@users.noreply.github.com>
2020-06-09 17:12:51 +0200

    removed file include for activate and cancel

--- billmate-checkout-for-woocommerce.php
+++ billmate-checkout-for-woocommerce.php
@@ -125,8 +125,6 @@ if ( ! class_exists( 'Billmate_Checkout_For_WooCommerce' ) ) {
 			include_once BILLMATE_CHECKOUT_PATH . '/classes/requests/checkout/post/class-bco-request-init-checkout.php';
 			include_once BILLMATE_CHECKOUT_PATH . '/classes/requests/checkout/get/class-bco-request-get-checkout.php';
 			include_once BILLMATE_CHECKOUT_PATH . '/classes/requests/checkout/get/class-bco-request-get-payment.php';
-			include_once BILLMATE_CHECKOUT_PATH . '/classes/requests/order-management/post/class-bco-request-activate-payment.php';
-			include_once BILLMATE_CHECKOUT_PATH . '/classes/requests/order-management/post/class-bco-request-cancel-payment.php';
 
 			// Request Helpers.
 			include_once BILLMATE_CHECKOUT_PATH . '/classes/requests/helpers/class-bco-payment-data-helper.php';
