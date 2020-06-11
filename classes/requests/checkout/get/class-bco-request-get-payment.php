556c382ab7085a55b9ab2b62f298b4b5c1d630c0
Kristian Andersson <32387117+Kristian-Andersson@users.noreply.github.com>
2020-06-11 14:37:03 +0200

    Updated variable name to correct name

--- classes/requests/checkout/get/class-bco-request-get-payment.php
+++ classes/requests/checkout/get/class-bco-request-get-payment.php
@@ -17,18 +17,18 @@ class BCO_Request_Get_Payment extends BCO_Request {
 	/**
 	 * Makes the request.
 	 *
-	 * @param string $bco_order_number Billmate order number.
+	 * @param string $bco_transaction_id Billmate transaction id.
 	 * @return array
 	 */
-	public function request( $bco_order_number ) {
+	public function request( $bco_transaction_id ) {
 		$request_url  = $this->base_url;
-		$request_args = apply_filters( 'bco_get_payment_args', $this->get_request_args( $bco_order_number ) );
+		$request_args = apply_filters( 'bco_get_payment_args', $this->get_request_args( $bco_transaction_id ) );
 
 		$response = wp_remote_request( $request_url, $request_args );
 		$code     = wp_remote_retrieve_response_code( $response );
 
 		// Log the request.
-		$log = BCO_Logger::format_log( $bco_order_number, 'GET', 'BCO get payment', $request_args, json_decode( wp_remote_retrieve_body( $response ), true ), $code );
+		$log = BCO_Logger::format_log( $bco_transaction_id, 'GET', 'BCO get payment', $request_args, json_decode( wp_remote_retrieve_body( $response ), true ), $code );
 		BCO_Logger::log( $log );
 
 		$formated_response = $this->process_response( $response, $request_args, $request_url );
@@ -38,11 +38,11 @@ class BCO_Request_Get_Payment extends BCO_Request {
 	/**
 	 * Gets the request body.
 	 *
-	 * @param string $bco_order_number Billmate order number.
+	 * @param string $bco_transaction_id Billmate transaction id.
 	 * @return array
 	 */
-	public function get_body( $bco_order_number ) {
-		$data         = $this->get_request_data( $bco_order_number );
+	public function get_body( $bco_transaction_id ) {
+		$data         = $this->get_request_data( $bco_transaction_id );
 		$request_body = array(
 			'credentials' => array(
 				'id'   => $this->id,
@@ -58,24 +58,24 @@ class BCO_Request_Get_Payment extends BCO_Request {
 	/**
 	 * Gets the request args for the API call.
 	 *
-	 * @param string $bco_order_number Billmate order number.
+	 * @param string $bco_transaction_id Billmate transaction id.
 	 * @return array
 	 */
-	public function get_request_args( $bco_order_number ) {
+	public function get_request_args( $bco_transaction_id ) {
 		return array(
 			'headers' => $this->get_headers(),
 			'method'  => 'POST',
-			'body'    => wp_json_encode( $this->get_body( $bco_order_number ) ),
+			'body'    => wp_json_encode( $this->get_body( $bco_transaction_id ) ),
 		);
 	}
 
 	/**
 	 * Get needed data for the request.
 	 *
-	 * @param string $bco_order_number Billmate order number.
+	 * @param string $bco_transaction_id Billmate transaction id.
 	 * @return array
 	 */
-	public function get_request_data( $bco_order_number ) {
-		return array( 'number' => $bco_order_number );
+	public function get_request_data( $bco_transaction_id ) {
+		return array( 'number' => $bco_transaction_id );
 	}
 }
