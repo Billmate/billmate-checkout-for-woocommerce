50236fbe5868b04d21b80b66ff0e0c03d2431875
Kristian Andersson <32387117+Kristian-Andersson@users.noreply.github.com>
2020-07-01 18:44:49 +0200

    Add ajax functionality for updating wc customer if we have new country or zip

--- classes/class-bco-ajax.php
+++ classes/class-bco-ajax.php
@@ -25,9 +25,10 @@ class BCO_AJAX extends WC_AJAX {
 	 */
 	public static function add_ajax_events() {
 		$ajax_events = array(
-			'bco_wc_update_checkout'  => true,
-			'bco_wc_get_checkout'     => true,
-			'bco_wc_checkout_success' => true,
+			'bco_wc_update_checkout'                => true,
+			'bco_wc_get_checkout'                   => true,
+			'bco_wc_iframe_shipping_address_change' => true,
+			'bco_wc_checkout_success'               => true,
 		);
 		foreach ( $ajax_events as $ajax_event => $nopriv ) {
 			add_action( 'wp_ajax_woocommerce_' . $ajax_event, array( __CLASS__, $ajax_event ) );
@@ -75,7 +76,7 @@ class BCO_AJAX extends WC_AJAX {
 				}
 
 				// Update order.
-				$billmate_order = BCO_WC()->api->request_update_checkout( WC()->session->get( 'bco_wc_number' ) );
+				$billmate_order = BCO_WC()->api->request_update_checkout( null, WC()->session->get( 'bco_wc_number' ) );
 				// If the update failed - reload the checkout page and display the error.
 				if ( ! $billmate_order ) {
 					wp_send_json_error();
@@ -102,6 +103,7 @@ class BCO_AJAX extends WC_AJAX {
 		}
 
 		$billmate_checkout = BCO_WC()->api->request_get_checkout( WC()->session->get( 'bco_wc_hash' ) );
+
 		if ( ! $billmate_checkout ) {
 			wp_send_json_error( $billmate_checkout );
 			wp_die();
@@ -115,6 +117,82 @@ class BCO_AJAX extends WC_AJAX {
 		wp_die();
 	}
 
+
+	/**
+	 * Shipping address change.
+	 *
+	 * @return void
+	 */
+	public static function bco_wc_iframe_shipping_address_change() {
+		$nonce = isset( $_POST['nonce'] ) ? sanitize_key( $_POST['nonce'] ) : '';
+		if ( ! wp_verify_nonce( $nonce, 'bco_wc_iframe_shipping_address_change' ) ) { // Input var okay.
+			wp_send_json_error( 'bad_nonce' );
+			exit;
+		}
+
+		wc_maybe_define_constant( 'WOOCOMMERCE_CHECKOUT', true );
+
+		$bco_payment_number = WC()->session->get( 'bco_wc_number' );
+
+		// Check if we have a Billmate payment number.
+		if ( empty( $bco_payment_number ) ) {
+			wc_add_notice( 'Billmate payment number is missing.', 'error' );
+			wp_send_json_error();
+			wp_die();
+		}
+
+		$customer_data    = array();
+		$address_data     = ( isset( $_REQUEST['address'] ) ) ? array_map( 'sanitize_text_field', wp_unslash( $_REQUEST['address'] ) ) : null;
+		$shipping_address = ( isset( $_REQUEST['address']['shippingAddress'] ) ) ? array_map( 'sanitize_text_field', wp_unslash( $_REQUEST['address']['shippingAddress'] ) ) : null;
+		$billing_address  = ( isset( $_REQUEST['address']['billingAddress'] ) ) ? array_map( 'sanitize_text_field', wp_unslash( $_REQUEST['address']['billingAddress'] ) ) : null;
+
+		// If we have shipping zip and country, use it for calculation. Else use billing zip and country for calculation.
+		if ( ! empty( $address_data['shippingZip'] ) && ! empty( $address_data['shippingCountry'] ) ) {
+			$shipping_zip     = $address_data['shippingZip'];
+			$shipping_country = strtoupper( $address_data['shippingCountry'] );
+
+		} elseif ( ! empty( $address_data['billingZip'] ) && ! empty( $address_data['billingCountry'] ) ) {
+			$billing_zip     = $address_data['billingZip'];
+			$billing_country = strtoupper( $address_data['billingCountry'] );
+		}
+
+		if ( ! empty( $shipping_zip ) ) {
+			$customer_data['shipping_postcode'] = $shipping_zip;
+			$customer_data['billing_postcode']  = $shipping_zip;
+		} elseif ( ! empty( $billing_zip ) ) {
+			$customer_data['shipping_postcode'] = $billing_zip;
+			$customer_data['billing_postcode']  = $billing_zip;
+		}
+
+		if ( ! empty( $shipping_country ) ) {
+			$customer_data['shipping_country'] = $shipping_country;
+			$customer_data['billing_country']  = $shipping_country;
+		} elseif ( ! empty( $billing_country ) ) {
+			$customer_data['shipping_country'] = $billing_country;
+			$customer_data['billing_country']  = $billing_country;
+		}
+
+		WC()->customer->set_props( $customer_data );
+		WC()->customer->save();
+
+		WC()->cart->calculate_shipping();
+		WC()->cart->calculate_totals();
+
+		$billmate_order = BCO_WC()->api->request_update_checkout( null, $bco_payment_number );
+
+		if ( false === $billmate_order ) {
+			wp_send_json_error();
+			wp_die();
+		}
+
+		wp_send_json_success(
+			array(
+				'billing_address'  => $billing_address,
+				'shipping_address' => $shipping_address,
+			)
+		);
+		wp_die();
+	}
 
 	/**
 	 * Checkout success.
