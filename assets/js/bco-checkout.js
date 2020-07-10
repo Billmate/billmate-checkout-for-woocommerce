e158367f4364ed7231c660cb3579c377e4418182
Kristian Andersson <32387117+Kristian-Andersson@users.noreply.github.com>
2020-07-10 14:30:28 +0200

    fix for #2

--- assets/js/bco-checkout.js
+++ assets/js/bco-checkout.js
@@ -1,42 +1,39 @@
 jQuery(function($) {
-    const bco_wc = {
+	const bco_wc = {
 
-        update: function() {
+		update: function() {
+			/**
+			 * Refresh checkout in iframe
+			 * In this example the name of iframe that contain Billmate Checkout have the name checkout_iframe
+			 */
+			checkout_iframe.postMessage('update', '*');
+		},
 
-            /**
-             * Refresh checkout in iframe
-             * In this example the name of iframe that contain Billmate Checkout have the name checkout_iframe
-             */
-            checkout_iframe.postMessage('update', '*');
-        },
-    
-        lock: function() {
-    
-            /**
-             * Lock Billmate checkout from customer interactions
-             * Will display loading animation and is used when the store is working on the order, for example when cart is updated
-             */
-            checkout_iframe.postMessage('lock', '*');
-        },
-    
-        unlock: function() {
-    
-            /**
-             * If checkout is locked, remove loading animation and customer can continue to interact with Billmate Checkout
-             * Is used when store is done with work that affect the order and Billmate Checkout, for example when cart is updated
-             */
-            checkout_iframe.postMessage('unlock', '*');
-        },
+		lock: function() {
+			/**
+			 * Lock Billmate checkout from customer interactions
+			 * Will display loading animation and is used when the store is working on the order, for example when cart is updated
+			 */
+			checkout_iframe.postMessage('lock', '*');
+		},
 
-        handleEvent: function(event) {
-            if(event.origin != "") {
-                try {
+		unlock: function() {
+			/**
+			 * If checkout is locked, remove loading animation and customer can continue to interact with Billmate Checkout
+			 * Is used when store is done with work that affect the order and Billmate Checkout, for example when cart is updated
+			 */
+			checkout_iframe.postMessage('unlock', '*');
+		},
+
+		handleEvent: function(event) {
+			if(event.origin != "") {
+				try {
 					var json = JSON.parse(event.data);
-                } catch (e) {
-                    return;
-                }
-    
-                switch (json.event) {
+				} catch (e) {
+					return;
+				}
+
+				switch (json.event) {
 					case 'purchase_initialized':
 						/**
 						 * When an end user clicks on the "Purchase button".
@@ -50,9 +47,8 @@ jQuery(function($) {
 								bco_wc.unlock();
 							}
 						});
-
 						break;
-                    case 'address_selected':
+					case 'address_selected':
 						if ( 'checkout' !== bco_wc_params.checkout_flow ) {
 							return;
 						}
@@ -78,7 +74,7 @@ jQuery(function($) {
 								billingCountry = json.data.Customer.Billing.country;
 								bco_wc.addressData.billingAddress = json.data.Customer.Billing;
 							}
-							
+
 							if ( bco_wc.addressData.shippingZip === shippingZip ) {
 								return;
 							}
@@ -100,25 +96,25 @@ jQuery(function($) {
 							}
 							bco_wc.addressData.updateNeeded = 'yes';
 						}
-		
+
 						bco_wc.addressData.billingZip = billingZip;
 						bco_wc.addressData.billingCountry = billingCountry;
 						bco_wc.addressData.shippingZip = shippingZip;
 						bco_wc.addressData.shippingCountry = shippingCountry;
 
 
-                        /**
-                         * Customer adress have been set or updated
-                         */
-						 $('#jsLog').append('address_selected<br />');
+						/**
+						 * Customer adress have been set or updated
+						 */
+						$('#jsLog').append('address_selected<br />');
 
-						 if ( 'yes' === bco_wc.addressData.updateNeeded ) {
+						if ( 'yes' === bco_wc.addressData.updateNeeded ) {
 							$( '.woocommerce-checkout-review-order-table' ).block({
 								message: null,
 								overlayCSS: {
 									background: '#fff',
 									opacity: 0.6
-									}
+								}
 							});
 							$.ajax(
 								{
@@ -133,7 +129,6 @@ jQuery(function($) {
 										bco_wc.setCustomerData(response.data);
 										// All good trigger update_checkout event.
 										$( 'body' ).trigger( 'update_checkout' );
-										
 									},
 									error: function() {
 										console.log( response );
@@ -145,83 +140,72 @@ jQuery(function($) {
 								}
 							);
 						}
-                        break;
-                    case 'payment_method_selected':
-
-                        /**
-                         * Payment method is selected
-                         */
-                         $('#jsLog').append('payment_method_selected<br />');
-    
-                        break;
-                    case 'checkout_success':
-                        
-                        /**
-                         * Order is paid and customer are not already redirected to accepturl
-                         * Store decideds what to do with order and if redirect customer
-                         */
-                         $('#jsLog').append('checkout_success<br />');
-							if ( 'checkout' === bco_wc_params.checkout_flow ) {
-								var redirectUrl = sessionStorage.getItem( 'billmateRedirectUrl' );
-								console.log(redirectUrl);
-								if( redirectUrl ) {
+						break;
+					case 'payment_method_selected':
+						/**
+						 * Payment method is selected
+						 */
+						$('#jsLog').append('payment_method_selected<br />');
+						break;
+					case 'checkout_success':
+						/**
+						 * Order is paid and customer are not already redirected to accepturl
+						 * Store decideds what to do with order and if redirect customer
+						 */
+						$('#jsLog').append('checkout_success<br />');
+						if ( 'checkout' === bco_wc_params.checkout_flow ) {
+							var redirectUrl = sessionStorage.getItem( 'billmateRedirectUrl' );
+							console.log(redirectUrl);
+							if( redirectUrl ) {
 								window.location.href = redirectUrl;
-								}
-							} else {
-								$.ajax(
-								  {
-									  url: bco_wc_params.checkout_success_url,
-									  type: 'POST',
-									  dataType: 'json',
-									  data: {
-										  nonce: bco_wc_params.checkout_success_nonce
-									  },
-									  success: function() {
-									  },
-									  error: function() {
-									  },
-									  complete: function( data ) {
-										  window.location.href = data.responseJSON.data.bco_wc_received_url;
-									  }
-								  }
-							  );
 							}
-    
-                        break;
-                    case 'content_height':
-                        
-                        /**
-                         * The height of checkout, parent can use height to set iframe height when height changes
-                         */
-                        $(document).find('#checkout').height(json.data);
-                        $('#jsLog').append('content_height: ' + json.data + '<br />');
-    
-                        break;
-                    case 'content_scroll_position':
-    
-                        /**
-                         * When checkout iframe page is scrolled 
-                         * If iframe have same height as Billmate Checkout the store can do the scrolling 
-                         */
-                        window.latestScroll = $(document).find( "#checkout" ).offset().top + json.data;
-                        $('html, body').animate({scrollTop: $(document).find( "#checkout" ).offset().top + json.data}, 400);
-    
-                        $('#jsLog').append('content_scroll_position: ' + json.data + '<br />');
-    
-                        break;
-                    case 'checkout_loaded':
-    
-                        $('#jsLog').append('checkout_loaded<br />');
-    
-                        break;
-                    default:
-                        break;
-    
-                }
-            }
-        },
+						} else {
+							$.ajax(
+								{
+									url: bco_wc_params.checkout_success_url,
+									type: 'POST',
+									dataType: 'json',
+									data: {
+										nonce: bco_wc_params.checkout_success_nonce
+									},
+									success: function() {
+									},
+									error: function() {
+									},
+									complete: function( data ) {
+										window.location.href = data.responseJSON.data.bco_wc_received_url;
+									}
+								}
+							);
+						}
+						break;
+					case 'content_height':
+						/**
+						 * The height of checkout, parent can use height to set iframe height when height changes
+						 */
+						$(document).find('#checkout').height(json.data);
+						$('#jsLog').append('content_height: ' + json.data + '<br />');
+						break;
+					case 'content_scroll_position':
+						/**
+						 * When checkout iframe page is scrolled 
+						 * If iframe have same height as Billmate Checkout the store can do the scrolling 
+						 */
+						window.latestScroll = $(document).find( "#checkout" ).offset().top + json.data;
+						$('html, body').animate({scrollTop: $(document).find( "#checkout" ).offset().top + json.data}, 400);
 
-        bodyEl: $('body'),
+						$('#jsLog').append('content_scroll_position: ' + json.data + '<br />');
+						break;
+					case 'checkout_loaded':
+						$('#jsLog').append('checkout_loaded<br />');
+						break;
+					default:
+					break;
+				}
+			}
+		},
+
+		bodyEl: $('body'),
 		checkoutFormSelector: 'form.checkout',
 		shippingUpdated: false,
 
@@ -251,7 +235,7 @@ jQuery(function($) {
 		extraFieldsSelectorText: 'div#bco-extra-checkout-fields input[type="text"], div#bco-extra-checkout-fields input[type="password"], div#bco-extra-checkout-fields textarea, div#bco-extra-checkout-fields input[type="email"], div#bco-extra-checkout-fields input[type="tel"]',
 		extraFieldsSelectorNonText: 'div#bco-extra-checkout-fields select, div#bco-extra-checkout-fields input[type="radio"], div#bco-extra-checkout-fields input[type="checkbox"], div#bco-extra-checkout-fields input.checkout-date-picker, input#terms input[type="checkbox"]',
 
-        updateBillmateCheckout: function() {
+		updateBillmateCheckout: function() {
 			$('.woocommerce-checkout-review-order-table').block({
 				message: null,
 				overlayCSS: {
@@ -325,7 +309,7 @@ jQuery(function($) {
 				$( '#billing_email' ).val( ( ( 'email' in data.billing_address ) ? data.billing_address.email : '' ) );
 				$( '#billing_country' ).val( ( ( 'country' in data.billing_address ) ? data.billing_address.country.toUpperCase() : '' ) );
 			}
-			
+
 			if ( data.shipping_address !== null ) {
 				$( '#ship-to-different-address-checkbox' ).prop( 'checked', true);
 
@@ -344,27 +328,27 @@ jQuery(function($) {
 		hashChange: function() {
 			console.log('hashchange');
 			var currentHash = location.hash;
-            var splittedHash = currentHash.split("=");
-            console.log(splittedHash[0]);
-            console.log(splittedHash[1]);
-            if(splittedHash[0] === "#billmate-success"){
+			var splittedHash = currentHash.split("=");
+			console.log(splittedHash[0]);
+			console.log(splittedHash[1]);
+			if(splittedHash[0] === "#billmate-success"){
 				$( 'body' ).trigger( 'bco_order_validation', true );
 				var response = JSON.parse( atob( splittedHash[1] ) );
-                console.log('response.redirect_url');
-                console.log(response.redirect_url);
+				console.log('response.redirect_url');
+				console.log(response.redirect_url);
 				sessionStorage.setItem( 'billmateRedirectUrl', response.redirect_url );
 				$('form.checkout').removeClass( 'processing' ).unblock();
-            }
+			}
 		},
 
 		errorDetected: function() {
 			$( 'body' ).trigger( 'bco_order_validation', false );
 		},
 
-        /*
-		 * Document ready function. 
-		 * Runs on the $(document).ready event.
-		 */
+		/*
+		* Document ready function. 
+		* Runs on the $(document).ready event.
+		*/
 		documentReady: function() {
 			bco_wc.moveExtraCheckoutFields();
 
@@ -375,9 +359,9 @@ jQuery(function($) {
 					$('#bco-iframe').addClass('bco-two-column-checkout-right');
 				}
 			}
-        },
+		},
 
-        // When "Change to another payment method" is clicked.
+		// When "Change to another payment method" is clicked.
 		changeFromBCO: function(e) {
 			e.preventDefault();
 
@@ -435,10 +419,10 @@ jQuery(function($) {
 				});
 			}
 		},
-        
-        /**
-		 * Moves all non standard fields to the extra checkout fields.
-		 */
+
+		/**
+		* Moves all non standard fields to the extra checkout fields.
+		*/
 		moveExtraCheckoutFields: function() {
 			// Move order comments.
 			$( '.woocommerce-additional-fields' ).appendTo( '#bco-extra-checkout-fields' );
@@ -460,9 +444,9 @@ jQuery(function($) {
 			}
 		},
 
-        /*
-		 * Check if our gateway is the selected gateway.
-		 */
+		/*
+		* Check if our gateway is the selected gateway.
+		*/
 		checkIfSelected: function() {
 			if (bco_wc.paymentMethodEl.length > 0) {
 				bco_wc.paymentMethod = bco_wc.paymentMethodEl.filter(':checked').val();
@@ -474,30 +458,29 @@ jQuery(function($) {
 		},
 
 
-        init: function() {
+		init: function() {
 			window.addEventListener("message", bco_wc.handleEvent);
-            // Check if Billmate is the selected payment method before we do anything.
+			// Check if Billmate is the selected payment method before we do anything.
 			if( bco_wc.checkIfSelected() ) {
-                $(document).ready( bco_wc.documentReady() );
+				$(document).ready( bco_wc.documentReady() );
 
-                // Change from BCO.
-                bco_wc.bodyEl.on('click', bco_wc.selectAnotherSelector, bco_wc.changeFromBCO);
+				// Change from BCO.
+				bco_wc.bodyEl.on('click', bco_wc.selectAnotherSelector, bco_wc.changeFromBCO);
 
 				if ( 'checkout' === bco_wc_params.checkout_flow ) {
 					// Update Billmate payment.
 					bco_wc.bodyEl.on('updated_checkout', bco_wc.updateBillmateCheckout); 					
 				}
-                
-                // Hashchange.
-                $( window ).on('hashchange', bco_wc.hashChange);
-                
+
+				// Hashchange.
+				$( window ).on('hashchange', bco_wc.hashChange);
+
 				// Error detected.
 				$( document.body ).on( 'checkout_error', bco_wc.errorDetected );
-            }
-            bco_wc.bodyEl.on('change', 'input[name="payment_method"]', bco_wc.maybeChangeToBCO);
+			}
+			bco_wc.bodyEl.on('change', 'input[name="payment_method"]', bco_wc.maybeChangeToBCO);
 			bco_wc.bodyEl.on( 'click', bco_wc.selectAnotherSelector, bco_wc.changeFromBCO );
-        },
-    }
-    bco_wc.init();
+		},
+	}
+	bco_wc.init();
 });
-
