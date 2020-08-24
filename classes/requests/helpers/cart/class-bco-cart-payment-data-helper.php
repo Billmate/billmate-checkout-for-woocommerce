0dcab2ed54da588a187932c9d92518570f09e883
Niklas Högefjord <niklas@krokedil.se>
2020-08-24 16:34:57 +0200

    Only use sv as locale. Billmate doesn't support other languages at the moment

--- classes/requests/helpers/cart/class-bco-cart-payment-data-helper.php
+++ classes/requests/helpers/cart/class-bco-cart-payment-data-helper.php
@@ -56,14 +56,8 @@ class BCO_Cart_Payment_Data_Helper {
 	 * @return string
 	 */
 	public static function get_language() {
-		$iso_code = explode( '_', get_locale() );
-		if ( in_array( $iso_code[0], array( 'sv', 'da', 'no', 'en' ), true ) ) {
-			$lang = $iso_code[0];
-		} else {
-			$lang = 'en';
-		}
-
-		return $lang;
+		// Swedish is the only supported language at the moment.
+		return 'sv';
 	}
 
 	/**
