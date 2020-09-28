d3cbac145852c133d06af2dd437cf79ed41fbaf1
Niklas Högefjord <niklas@krokedil.se>
2020-09-28 09:01:27 +0200

    Tweak in how checkout page is blocked when order is finalized (during BankID signing)

--- assets/js/bco-checkout.js
+++ assets/js/bco-checkout.js
@@ -341,7 +341,13 @@ jQuery(function($) {
 				console.log('response.redirect_url');
 				console.log(response.redirect_url);
 				sessionStorage.setItem( 'billmateRedirectUrl', response.redirect_url );
-				$( 'body' ).append( $( '<div class="billmate-success-modal"><div class="billmate-success-modal-content">' + bco_wc_params.success_text + '</div></div>' ) );
+				$('#bco-wrapper').block({
+					message: null,
+					overlayCSS: {
+						background: '#fff',
+						opacity: 0.6
+					}
+				});
 				$('form.checkout').removeClass( 'processing' ).unblock();
 			}
 		},
